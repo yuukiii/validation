7dab14084c0a15c5fad7bb94efd03ebaf587d549
garfieldheron <hansismydog01>
2022-09-25 03:01:42 -0400

    dev null

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-aws ec2 describe-key-pairs | grep 'KeyName' | grep -c 'awesome-key'
-aws ec2 describe-security-groups | grep 'GroupName' | grep -c 'awesome-sq'
-aws ec2 describe-instances --filters='Name=instance-state-name,Values=running' | grep -c 'InstanceId'
+aws ec2 describe-instances --filters='Name=instance-state-name,Values=running' | grep -c 'InstanceId' >/dev/null 2>&1 && echo OK
