9d8bf2a7ebea6d74113ab6d47e83b9de08bb7407
garfieldheron <hansismydog01>
2022-09-25 03:19:22 -0400

    check security group

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-aws ec2 describe-instances --filters='Name=instance-state-name,Values=running' | grep -c 'InstanceId' >/dev/null 2>&1 && echo OK
+#aws ec2 describe-key-pairs | grep 'KeyName' | grep -c 'awesome-key'
+aws ec2 describe-security-groups | grep 'GroupName' | grep -c 'awesome-sq'  >/dev/null 2>&1 && echo OK
+#aws ec2 describe-instances --filters='Name=instance-state-name,Values=running' | grep -c 'InstanceId'
